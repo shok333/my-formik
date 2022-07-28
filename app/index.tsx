48a387463806ee35d3f58cc3999af768f94127c0
Dmitriy Gerasimov <gerasimov@action-media.ru>
2022-07-28 18:14:37 +0300

    add formik library

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,52 @@
-import React, { FC, useEffect } from "react";
+import React from 'react';
+import { Formik } from 'formik';
 
-const MyApp: FC = () => {
-  useEffect(() => {
-    console.log('Hello my formik')
-  }, [])
+const Basic = () => (
+  <div>
+    <h1>Anywhere in your app!</h1>
+    <Formik
+      initialValues={{ email: '', password: '' }}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 400);
+      }}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isSubmitting,
+        /* and other goodies */
+      }) => (
+        <form onSubmit={handleSubmit}>
+          <input
+            type="email"
+            name="email"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.email}
+          />
+          {errors.email && touched.email && errors.email}
+          <input
+            type="password"
+            name="password"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.password}
+          />
+          {errors.password && touched.password && errors.password}
+          <button type="submit" disabled={isSubmitting}>
+            Submit
+          </button>
+        </form>
+      )}
+    </Formik>
+  </div>
+);
 
-  return (
-    <div>123321</div>
-  )
-}
-
-export default MyApp
+export default Basic;
